232e8b0510f16660b42bfef481550c4f1cbb83c4
Giovani Aguirre <me@its-g.io>
2020-02-10 14:35:13 -0600

    Created store and reducer.

--- src/store.js
+++ src/store.js
@@ -1,1 +1,27 @@
+import createStore from "redux";
 
+const initialState = {
+  recName: "",
+  recCategory: ""
+}
+
+const update_recName = "update_recName";
+const update_recCategory = "update_recCategory";
+
+function reducer(state=initialState, action) {
+  switch(action.type) {
+    case update_recName:
+      return {
+        ...state,
+        recName: action.payload
+      }
+    case update_recCategory:
+      return {
+        ...state,
+        recCategory: action.payload
+      }
+    default: return state;
+  }
+}
+
+export default createStore(reducer);
